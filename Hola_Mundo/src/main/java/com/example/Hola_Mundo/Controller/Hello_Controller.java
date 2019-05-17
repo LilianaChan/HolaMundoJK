55824187eeab71ee1f9bba69239ff4f0e0de86b8
LilianaChan <lilianachancaamal@gmail.com>
2019-05-17 14:48:28 -0500

    primer commit

---
+++ Hola_Mundo/src/main/java/com/example/Hola_Mundo/Controller/Hello_Controller.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.Hola_Mundo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author USERPS
+ */
+@Controller 
+public class Hello_Controller {
+    
+    @GetMapping({"/","hello"})
+    public String hello(){
+        
+        return "hello";
+    }
+    
+    @RequestMapping(value = "/world",method = RequestMethod.GET)
+    public String world(){
+        return "world";
+    }
+}
---
+++ Hola_Mundo/src/main/webapp/WEB-INF/views/world.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : world
+    Created on : 14/05/2019, 02:25:22 PM
+    Author     : USERPS
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World! from world.jsp</h1>
+    </body>
+</html>
